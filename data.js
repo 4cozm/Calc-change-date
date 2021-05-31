3109efcb1910e27c1b1141b1e637f411c10037e4
4cozm <49065386+4cozm@users.noreply.github.com>
2021-05-31 16:01:05 +0900

    ㅁㄴㅇ

--- data.js
+++ data.js
@@ -5,11 +5,11 @@ var today=day.getTime();
 document.querySelector("#today").innerHTML=(today_Month+"월"+today_Date+"일 기준으로 개통시");
 var D120=today+(121*86400000);
 var D180=today+(182*86400000);
-var D275=today+(275*86400000);
+var D305=today+(305*86400000);
 
 var Yo_Hal=new Date(D120);
 var Gong_Si=new Date(D180);
-var internet=new Date(D275);
+var internet=new Date(D305);
 
 document.querySelector("#select25per").innerHTML="<h2>요금할인 개통시</h2>"+Yo_Hal.getFullYear()+"년"+(Yo_Hal.getMonth()+1)+"월"+Yo_Hal.getDate()+"일 요금제 변경 가능";
 document.querySelector("#gong").innerHTML="<h2>공시지원금 개통시</h2>"+Gong_Si.getFullYear()+"년"+(Gong_Si.getMonth()+1)+"월"+Gong_Si.getDate()+"일 요금제 변경 가능";
